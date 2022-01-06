560d2c4369320378df2700a3b660d46a4ffa24fb
Benjamin Robbins <benro2211@gmail.com>
2022-01-05 23:37:52 -0700

    amazon scraper output to csv

--- beautiful_soup.py
+++ beautiful_soup.py
@@ -1,19 +1,34 @@
 #!python3
 import bs4
 import requests
+import re
 #TODO make functions for various scraping desires
+#https://www.geeksforgeeks.org/scraping-amazon-product-information-using-beautiful-soup/
 
 
 def get_price(product_url):
-    try:
         headers = ({'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36','Accept-Language': 'en-US, en;q=0.5'})
         ro = requests.get(product_url,headers = headers)
+        ro.raise_for_status()
+        #print(ro.text)
+        t = ro.text
+        find_span = re.compile(r'<span\sid="(.*?)"')
+        fo = find_span.findall(t)
         po = bs4.BeautifulSoup(ro.content,'html.parser')
         po_pretty = bs4.BeautifulSoup(po.prettify(),'html.parser')
-        pr = po_pretty.find("span", attrs={'id': 'newBuyBoxPrice'}).text.strip()
-        return "Used price: {}".format(pr)
-    except:
-        ro.raise_for_status()
+        nf = open('amazon.csv','a')
+        for i in fo:
+            pr = po_pretty.find("span", attrs={'id': i})
+            entry = (i+':'+pr.text.strip())
+            nf.write(entry)
+            nf.write('\n')
+        nf.close()
+
+                
+                
+                
+            
+        
 
 
-print(get_price("https://www.amazon.com/Automate-Boring-Stuff-Python-Programming/dp/1593275994"))
+print(get_price(r"https://www.amazon.com/Automate-Boring-Stuff-Python-2nd-dp-1593279922/dp/1593279922/ref=mt_other?_encoding=UTF8&me=&qid=1641447778"))
